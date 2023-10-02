5f85eb53fe41a722b64ccafa01c72ae7140ba9e8
EmreSafaCelik <safaemrecelik@gmail.com>
2023-10-02 16:04:47 +0300

    firestore/storage part 2

--- src/screens/UploadScreen.jsx
+++ src/screens/UploadScreen.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
     Text,
     View,
@@ -8,6 +8,7 @@ import {
     Platform,
     Alert,
     TextInput,
+    FlatList,
 } from 'react-native'
 
 import {
@@ -23,34 +24,53 @@ import { auth, firestore, storage } from '../utility/firebase'
 const UploadScreen = () => {
 
     const [name, setName] = useState('')
+    const [data, setData] = useState([])
+    const [yuklenmeyiGoster, setYuklenmeyiGoster] = useState(false)
 
-    const add = async (imageURL) => {
-        firestore().collection('tarifler').add({name: name, image: imageURL})
-    }
+    useEffect(() => {
+        fetchData()
+    }, [])
 
     const fetchData = async () => {
-        const data = await firestore().collection('kisiler').get()
-        console.log(data.docs[0].data())
+        setYuklenmeyiGoster(true)
+        const data = await firestore().collection('tarifler').get()
+        console.log(data.docs)
+        setData(data.docs)
+        setYuklenmeyiGoster(false)
+    }
+
+    const add = async (image, name) => {
+        const imageURL = await uploadImage(image)
+        console.log('add içerisindeki imageURL: ', imageURL)
+
+        if (imageURL) {
+            await firestore().collection('tarifler').add({image: imageURL, name: name})
+            fetchData()
+        } else {
+            console.log('imageURL boş')
+        }
     }
 
     const uploadImage = async (image) => {
-        console.log(image)
+        console.log('uploadImage içerisindeki image:', image)
+        if (!image) return
 
-        const uploadUri = Platform.OS === "ios" ? image.sourceURL : image.path
+        const uploadUri = Platform.OS === 'ios' ? image.sourceURL : image.path
         const filename = `${auth().currentUser.uid}_${Date.now()}.jpg`
-        const storageRef = storage().ref(`tarifler/${filename}`)
+        const storageRef = storage().ref(`recipes/${filename}`)
+
         await storageRef.putFile(uploadUri)
 
         try {
             const url = await storageRef.getDownloadURL()
             return url
         } catch (error) {
-            console.log(e)
+            console.log(error)
             return null
         }
     }
 
-    const choosePhoto = async () => {
+    const choosePhotoAndAdd = async () => {
         Alert.alert(
             "Bir seçim yapın",
             "Kamerayı mı açmak istersiniz Galeriyi mi?",
@@ -75,7 +95,7 @@ const UploadScreen = () => {
                             const image = await ImageCropPicker.openCamera({
                                 cropping: true,
                             })
-                            add(uploadImage(image))
+                            add(image, name)
                         }
                     }
                 },
@@ -83,41 +103,54 @@ const UploadScreen = () => {
                     text: "Galeriyi",
                     onPress: async () => {
                         const image = await ImageCropPicker.openPicker({})
-                        add(uploadImage(image))
+                        add(image, name)
                     }
                 }
             ]
         )
     }
 
+    const Header = () => {
+
+        return (
+            <View>
+                <TextInput
+                    value={name}
+                    onChangeText={setName}
+                    style={{borderWidth: 1}}
+                />
+                <TouchableOpacity
+                    onPress={choosePhotoAndAdd}
+                >
+                    <Text>
+                        Fotoğraf çek veya seç
+                    </Text>
+                </TouchableOpacity>
+            </View>
+        )
+    }
+
     return (
         <View>
-            <TextInput
-                value={name}
-                onChangeText={setName}
-                style={{borderWidth: 1}}
+            <FlatList
+                data={data}
+                ListHeaderComponent={Header}
+                onRefresh={fetchData}
+                refreshing={yuklenmeyiGoster}
+                renderItem={element => {
+                    console.log('element:', element)
+
+                    return (
+                        <View>
+                            <Text>{element?.item?.data()?.name}</Text>
+                            <Image
+                                source={{uri: element?.item?.data()?.image}}
+                                style={{width: '100%', height: 200}}
+                            />
+                        </View>
+                    )
+                }}
             />
-            <TouchableOpacity
-                onPress={add}
-            >
-                <Text>
-                    Ekleme butonu
-                </Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-                onPress={fetchData}
-            >
-                <Text>
-                    Veri çek
-                </Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-                onPress={choosePhoto}
-            >
-                <Text>
-                    Fotoğraf çek veya seç ve tarifi yükle
-                </Text>
-            </TouchableOpacity>
         </View>
 
     )
